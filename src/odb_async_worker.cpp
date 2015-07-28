7551c8448989fb78704483a96fcc570662681d56
Hessam Khoshniat <hkhoshniat@tosanidm.net>
2015-07-28 15:48:06 +0430

    odb_async_worker files added

---
+++ src/odb_async_worker.cpp
@@ -0,0 +1,87 @@
+#include "odb_async_worker.hpp"
+namespace oi
+{
+    odb_async_worker::odb_async_worker()
+    {
+        _state= odb_worker_base::state::NEW;
+        _db = nullptr;
+    }
+    void odb_async_worker::init(oi_database* db, 
+            const odb_worker_param& prm,
+            std::function<void(oi::exception)>  handler
+            ) throw(oi::exception)
+
+    {
+        
+        if(_state != odb_worker_base::state::NEW)
+        {
+            throw oi::exception(__FILE__, __FUNCTION__, 
+                    "invalid state(re-initialization or initialization of a finalized worker)");
+        }
+        if(db == nullptr)
+        {
+            throw oi::exception(__FILE__, __FUNCTION__, "database pointer is null");
+        }
+        if( prm.pool_size <= 0 || 
+                prm.commit_count <= 0 || 
+                prm.commit_timeout <= 0 || 
+                prm.max_que_size <= 0 ||
+                prm.pool_size > MAX_POOL_SIZE ||
+                prm.max_que_size> MAX_QUE_SIZE ||
+                prm.commit_timeout > MAX_COMMIT_TIMEOUT ||
+                handler == nullptr
+          )
+        {
+            std::stringstream sstr;
+            sstr << "invalid initialization parameter: " << prm.to_string().c_str()
+                << "  0 <= pool_size < " << MAX_POOL_SIZE
+                << "  0 <= commit_count < " << MAX_COMMIT_COUNT
+                << "  0 <= commit_timeout < " << MAX_COMMIT_TIMEOUT
+                << "  exception_handler != null ";
+            throw oi::exception(__FILE__, __FUNCTION__, sstr.str().c_str());
+        }
+        _exception_handler  = handler;
+        _init_param = prm;
+        _db = db;
+        _state = odb_worker_base::state::READY;
+    }
+
+    odb_stat odb_async_worker::get_stat()throw()
+    {
+        
+        std::map<std::string, odb_worker_base*>::iterator it;
+        odb_stat st, aggr_st;
+        if(_state == odb_worker_base::state::READY)
+        {
+            _workers_guard.lock();
+            {
+                for( it = _workers.begin(); it != _workers.end(); it++)
+                {
+                    st = it->second->get_stat();
+                    aggr_st = aggr_st + st;
+                }
+            }
+            _workers_guard.unlock();
+        }
+        return aggr_st;
+    }
+
+    void odb_async_worker::finalize()throw ()
+    {
+        
+        if(_state == odb_worker_base::state::READY)
+        {
+            std::map<std::string, odb_worker_base*>::iterator it;
+            _workers_guard.lock();
+            {
+                for( it = _workers.begin(); it != _workers.end(); it++)
+                {
+                    it->second->finalize();
+                    delete it->second;
+                }
+            }
+            _workers_guard.unlock();
+        }
+        _state = odb_worker_base::state::TERMINATED;
+    }
+}
---
+++ src/odb_async_worker.hpp
@@ -0,0 +1,57 @@
+#ifndef ODB_ASYNC_WORKER_HPP
+#define ODB_ASYNC_WORKER_HPP
+#include "odb_worker.hpp"
+namespace oi
+{
+    class odb_async_worker
+    {
+        private:
+            odb_worker_base::state _state;
+            std::map<std::string, odb_worker_base*> _workers;
+            std::mutex _workers_guard;
+            odb_worker_param _init_param;
+            oi_database * _db;
+            std::function<void(oi::exception)> _exception_handler;
+            
+            odb_async_worker(const odb_async_worker&);//is PRIVATE to avoid pass by value - no body_base
+            odb_async_worker& operator=(const odb_async_worker&); //is PRIVATE to avoid assigning - no body
+        public:
+            odb_async_worker();
+            void init(oi_database* db, const odb_worker_param& prm, std::function<void(oi::exception)>  handler ) throw(oi::exception);
+            odb_stat get_stat()throw();
+
+            template<typename T>
+                void persist(const T & obj)throw(oi::exception)
+                {
+                    
+                    if(_state != odb_worker_base::state::READY)
+                    {
+                        throw oi::exception(__FILE__, __FUNCTION__, "invalid use of un-initilized/finalized worker");
+                    }
+                    std::string t_name = typeid(T).name();
+                    std::map<std::string, odb_worker_base*>::iterator it;
+                    odb_worker<T> * wr = NULL;
+                    _workers_guard.lock();
+                    {
+                        it = _workers.find(t_name);
+                        if(it == _workers.end())
+                        {
+                            wr = new odb_worker<T>();
+                            wr->init(_db, _init_param, _exception_handler);
+                            _workers[t_name] = static_cast<odb_worker_base*>(wr);
+                            std::cerr << "new worker created for " << t_name << std::endl;
+                        }
+                        else
+                        {
+                            wr = static_cast< odb_worker<T>* >(it->second);
+                        }
+                    }
+                    _workers_guard.unlock();
+                    wr->persist(obj);
+                }
+
+            void finalize()throw();
+    };
+}
+#endif
+
